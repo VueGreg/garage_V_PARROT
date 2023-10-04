357bb4f0565ab2f155814932d73626d0a8bbf1e7
Gregory WOLFF <gregory.wolff.dev@gmail.com>
2023-10-04 17:50:42 +0200

    First commit with sql files for generate database

---
+++ dataSQL/table/horaires.sql
@@ -0,0 +1,20 @@
+USE v_parrot;
+
+CREATE TABLE `horaires` ( 
+    `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY , 
+    `jour_semaine`VARCHAR(10) NOT NULL ,
+    `h_debut_matin` DATE NOT NULL , 
+    `h_fin_matin` DATE NOT NULL , 
+    `h_debut_soir` DATE NOT NULL , 
+    `h_fin_soir` DATE NOT NULL 
+) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_general_ci; 
+
+INSERT INTO `horaires` (`id`, `jour_semaine`, `h_debut_matin`, `h_fin_matin`, `h_debut_soir`, `h_fin_soir`) 
+VALUES
+    (NULL, 'Lundi', '08:00:00', '12:00:00', '13:00:00', '20:00:00'),
+    (NULL, 'Mardi', '08:00:00', '12:00:00', '13:00:00', '20:00:00'),
+    (NULL, 'Mercredi', '08:00:00', '12:00:00', '13:00:00', '20:00:00'),
+    (NULL, 'Jeudi', '08:00:00', '12:00:00', '13:00:00', '20:00:00'),
+    (NULL, 'Vendredi', '08:00:00', '12:00:00', '13:00:00', '20:00:00'),
+    (NULL, 'Samedi', '08:00:00', '12:00:00', '13:00:00', '20:00:00'),
+    (NULL, 'Dimanche', '08:00:00', '12:00:00', '13:00:00', '20:00:00');
---
+++ dataSQL/table/temoignage.sql
@@ -0,0 +1,20 @@
+USE v_parrot;
+
+CREATE TABLE `temoignages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `date` DATE NOT NULL,
+  `nom` VARCHAR(50) NOT NULL,
+  `prenom` VARCHAR(50) NOT NULL,
+  `commentaire` TEXT NOT NULL,
+  `note` TINYINT NOT NULL,
+  `id_image` INT(11) NULL
+) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
+
+ALTER TABLE `temoignages`
+ADD CONSTRAINT `images` FOREIGN KEY (`id_image`) REFERENCES `images`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+
+INSERT INTO `temoignages` (`id`, `date`, `nom`, `prenom`, `commentaire`, `note`, `id_image`) 
+VALUES 
+    (NULL, '2023-10-04', 'De la Fontaine', 'Jean', 'Très bon garage, je recommande les yeux fermé', '5', NULL),
+    (NULL, '2023-09-26', 'Dupont', 'Georges', 'Très professionnel...cherche à faire le meilleur prix avec des pièces compatibles Toujours bien reçu Je recommande ce garage ', '5', NULL),
+    (NULL, '2023-08-12', 'Tapis', 'Bernard', 'Tres bon accueil, Travail consciencieux, dans les délais... vraiment très agréable ', '5', NULL);
